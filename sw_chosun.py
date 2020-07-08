11df57f91e6d0ddbaedbb3211570ba5a661a142d
sowmoo <43737763+sowmoo@users.noreply.github.com>
2020-07-09 00:41:34 +0900

    Update sw_chosun.py

--- sw_chosun.py
+++ sw_chosun.py
@@ -1,13 +1,12 @@
-import requests
+import requests #request용 모듈 
+from bs4 import BeautifulSoup #response된 html문장들을 python형식에 맞춰 다져줌 
 
-from bs4 import BeautifulSoup
-
-request = requests.get('http://sw.chosun.ac.kr/boardList.do?pageId=www42&boardId=NOTICE')
+request = requests.get('http://sw.chosun.ac.kr/boardList.do?pageId=www42&boardId=NOTICE') #크롤링 주소 
 html = request.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-my_titles = soup.select('#skip.content > div.content > div.siiru-boardWrap > div.board_list > div.board_list_body')
+my_titles = soup.select('#skip.content > div.content > div.siiru-boardWrap > div.board_list > div.board_list_body')  #다져진 전체html문장중 추출주소 
 
 for title in my titles:
        f = open('chosun/sw_chosun.txt',mode='wt',encoding='utf-8')
